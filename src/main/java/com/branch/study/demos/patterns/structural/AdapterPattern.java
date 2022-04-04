9bed0a5fd8c64b784a8819ed45a0a24432a2749a
BranchDon <dbczxwl@126.com>
2022-04-04 13:41:02 +0800

    更新

--- src/main/java/com/branch/study/demos/patterns/structural/AdapterPattern.java
+++ src/main/java/com/branch/study/demos/patterns/structural/AdapterPattern.java
@@ -1,14 +1,47 @@
 package com.branch.study.demos.patterns.structural;
 
 /**
- *
+ * Adapter实现用户需要的接口，并关联Adaptee（或者继承Adaptee）
+ * 对Adaptee中方法的输出进行处理，来匹配到用户需要的接口
+ * 
  * @author Administrator
  */
 public class AdapterPattern {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ChineseTranslator chineseTranslator = new ChineseTranslator(new Japanese());
+		chineseTranslator.speakChinese();
 	}
 
+}
+
+// Target
+interface Chinese {
+	void speakChinese();
+}
+
+// Adapter
+class ChineseTranslator implements Chinese {
+	private Japanese japanese;
+
+	public ChineseTranslator(Japanese japanese) {
+		this.japanese = japanese;
+	}
+
+	@Override
+	public void speakChinese() {
+		japanese.speakJapanese();
+		translateToChinese();
+	}
+
+	private void translateToChinese() {
+		System.out.println("中国翻译说中文");
+	}
+}
+
+// Adaptee
+class Japanese {
+	void speakJapanese() {
+		System.out.println("日本人说日语");
+	}
 }
--- src/main/java/com/branch/study/demos/patterns/structural/DecoratorPattern.java
+++ src/main/java/com/branch/study/demos/patterns/structural/DecoratorPattern.java
@@ -4,7 +4,7 @@ package com.branch.study.demos.patterns.structural;
  * Decorator：装饰器模式（包装类）
  * 扩展一个类可以通过，继承或者关联
  * 从类图上看是Decorator接口对Component接口的扩展
- * Decorator类继承Component接口并通过传入Component的另一个实现类，拷贝或部分拷贝原实现类中的功能
+ * Decorator类继承Component接口并通过传入Component的另一个实现类，拷贝或部分拷贝原实现类中的功能(希望调用者不发觉改变了类)
  * Decorator设置为abstract，可以更方便装饰类的扩展，Decorator者中关联的Component的接口而非实现类
  * Java io流中FilterInputStream抽象类的子类通过传入其他InputStream，进行功能扩展
  * 
@@ -34,7 +34,6 @@ class OldPhone implements Phone {
 	@Override
 	public void sendMessage() {
 		System.out.println("老式电话可以发短信");
-
 	}
 
 	@Override
@@ -59,7 +58,10 @@ abstract class SmartPhone implements Phone {
 
 	@Override
 	public void callUp() {
-		System.out.println("智能电话用自己的方式打电话");
+		System.out.println("智能电话可以用老电话的方式打电话");
+		phone.sendMessage();
+		// // 部分改变拷贝原实现类中的功能，这里注掉的部分自己来实现的callUp，而没有使用原实现类中的方法
+		// System.out.println("智能电话用自己的方式打电话");
 	}
 
 	public abstract void playGame();
