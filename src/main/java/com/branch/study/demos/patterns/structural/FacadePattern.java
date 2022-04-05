83a02b05144837e251513fac9a749c2e6471056e
BranchDon <dbczxwl@126.com>
2022-04-05 10:06:40 +0800

    更新

--- src/main/java/com/branch/study/demos/patterns/structural/FacadePattern.java
+++ src/main/java/com/branch/study/demos/patterns/structural/FacadePattern.java
@@ -1,9 +1,13 @@
 package com.branch.study.demos.patterns.structural;
 
-public class Facade {
+/**
+ * Facade：外观模式
+ * 
+ * @author Administrator
+ */
+public class FacadePattern {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
