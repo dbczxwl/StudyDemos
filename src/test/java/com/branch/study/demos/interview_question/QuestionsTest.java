b3ea41c16c2b6bc5479a3d2cc106a1196871b5fd
dbczxwl <dbczxwl@126.com>
2022-04-09 16:34:31 +0800

    更新

--- src/test/java/com/branch/study/demos/interview_question/QuestionsTest.java
+++ src/test/java/com/branch/study/demos/interview_question/QuestionsTest.java
@@ -12,27 +12,31 @@ public class QuestionsTest {
 	// 用于测试类的初始化
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		System.out.println("用于测试类的初始化");
 	}
 
 	// 用于测试类的清理
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		System.out.println("用于测试类的清理");
 	}
 
 	// 用于每个测试方法前的初始化
 	@Before
 	public void setUp() throws Exception {
+		System.out.println("用于每个测试方法前的初始化");
 	}
 
 	// 用于每个测试方法后的清理
 	@After
 	public void tearDown() throws Exception {
+		System.out.println("用于每个测试方法后的清理");
 	}
 
 	// 这是一个测试方法
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testMain() {
+		Questions.main(null);
 	}
 
 }
